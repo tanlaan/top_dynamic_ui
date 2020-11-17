e4f8e9620c95e1cf72385346ae1a3a97e0d75183
Chris Bolas <tanlaan33@gmail.com>
2020-11-16 22:12:26 -0800

    Add border + transition to menu

--- js/mobile.js
+++ js/mobile.js
@@ -3,16 +3,18 @@ const buttons = document.getElementsByClassName('hamburger')
 for (let i = 0; i < buttons.length; i += 1) {
     console.log('i got here')
     buttons[i].addEventListener('click', function hamburgerListener(e) {
-        const hiding = e.currentTarget.parentNode.getElementsByTagName('ul');
+        const parent = e.currentTarget.parentNode
+        const hiding = parent.getElementsByTagName('ul');
         for (let item of hiding) {
             let isHidden = item.classList.toggle('hidden');
+            
             if (!isHidden) {
                 item.style.opacity = '1';
             } else {
                 item.style.opacity = '0';
             }
         }
-        
+        parent.classList.toggle('selected')
 
     })
 }
